1c94353201d0ba3c09b887f6d0e86661bc95468f
GowthertG <houssamhaddouche777@gmail.com>
2025-01-21 18:59:54 +0100

    ADD and Update

--- 24_12_07nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-
--- Automatically compile using Python 3 after saving
--- vim.api.nvim_create_autocmd("BufWritePost", {
---   pattern = "*.py",
---   command = "!python3 %",
--- })
--- 24_12_07nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- disable trouble
-  { "stevearc/dressing.nvim",  enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/noice.nvim",        enabled = false },
-}
